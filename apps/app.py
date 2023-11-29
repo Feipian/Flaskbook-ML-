d490f2c862798b929a86737eb35ac830fb449d94
HongMing-Wang <superwang0603@gmail,com>
2023-11-29 17:54:59 +0800

    add csrf

--- apps/app.py
+++ apps/app.py
@@ -2,21 +2,28 @@ from flask import Flask
 from pathlib import Path
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.scrf import CSRFProtect
 
+csrf = CSRFProtect()
 db = SQLAlchemy()
 
 def create_app():
     # make a flask instance
     app = Flask(__name__)
+    app.debug = True
 
     
     # setting application
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}"
     app.config.from_mapping(
         SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # setting log outout
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="AuwzysU5sugKN7K6f",
     )
 
+    csrf.init_app(app)
     # connect SQLAlchemy and application
     db.init_app(app)
     # connect Migrate and application
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template
 
+# import db
+from apps.app import db
+#import user class
+from apps.crud.models import User
+
 # using Blueprint create crud application
 crud = Blueprint(
     "crud",
@@ -12,3 +17,8 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).get(2)
+    return "check log"
